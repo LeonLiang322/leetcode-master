842a15a9adafef2b5b997e2138bf1113761c64b3
Leon <ycliang.cn@outlook.com>
2024-06-05 20:00:23 +0100

    524 LongestWordInDictionaryThroughDeleting

--- java-note.md
+++ java-note.md
@@ -19,4 +19,21 @@ str.substring(1, 3);
 
 的结果为`bc`
 
+### compareTo
 
+定义字符串`str1`和`str2`，`str1.compareTo(str2)`：
+1. 返回两个字符串的asc码差值，从头开始一一比较，返回第一个不同字母的asc码差值
+    ``` java
+    String str1 = "aa";
+    String str2 = "ad";
+    System.out.println(str1.compareTo(str2)); // -3
+    ```
+2. 如果两个字符串长度不同，返回两个字符串长度差值
+    ``` java
+    String str1 = "aa";
+    String str2 = "a";
+    System.out.println(str1.compareTo(str2)); // 1
+    ```
+3. 返回正数，说明`str1 > str2`，返回负数，说明`str1 < str2`，返回0，说明`str1 = str2`
+4. `compareToIgnoreCase`忽略大小写
+5. `int`类型之间不能使用`compareTo`方法，需要转换为`String`类型。但是两个`Integer`类型可以
---
+++ leetcode/editor/cn/LongestWordInDictionaryThroughDeleting.java
@@ -0,0 +1,69 @@
+package leetcode.editor.cn;
+
+import java.util.List;
+
+/**
+ * Given a string s and a string array dictionary, return the longest string in
+ * the dictionary that can be formed by deleting some of the given string characters.
+ * If there is more than one possible result, return the longest word with the
+ * smallest lexicographical order. If there is no possible result, return the empty
+ * string.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: s = "abpcplea", dictionary = ["ale","apple","monkey","plea"]
+ * Output: "apple"
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * <p>
+ * Input: s = "abpcplea", dictionary = ["a","b","c"]
+ * Output: "a"
+ * <p>
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * <p>
+ * 1 <= s.length <= 1000
+ * 1 <= dictionary.length <= 1000
+ * 1 <= dictionary[i].length <= 1000
+ * s and dictionary[i] consist of lowercase English letters.
+ * <p>
+ * <p>
+ * Related Topics 数组 双指针 字符串 排序 👍 370 👎 0
+ */
+
+public class LongestWordInDictionaryThroughDeleting {
+    public static void main(String[] args) {
+        Solution solution = new LongestWordInDictionaryThroughDeleting().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        private boolean isSubStr(String s, String dic) {
+            int i = 0, j = 0;
+            while (i < s.length() && j < dic.length()) {
+                if (s.charAt(i) == dic.charAt(j)) j++;
+                i++;
+            }
+            return j == dic.length();
+        }
+
+        public String findLongestWord(String s, List<String> dictionary) {
+            String res = "";
+            for (String dic : dictionary) {
+                int l1 = res.length(), l2 = dic.length();
+                if (l1 > l2 || (l1 == l2 && res.compareTo(dic) < 0)) continue;
+                if (isSubStr(s, dic)) res = dic;
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
